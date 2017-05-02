a065724273bc6cb420adfa89862525961ae76397
aksrugiw <aksrugiw@interia.pl>
2017-05-02 12:38:00 +0200

    Add min and hour hand

--- CSS_Clock/script.js
+++ CSS_Clock/script.js
@@ -1,4 +1,6 @@
 const secHand = document.querySelector('.second-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
 
 function setDate() {
 	const now = new Date();
@@ -6,6 +8,14 @@ function setDate() {
 	const sec = now.getSeconds();
 	const secDegrees = ( sec / 60 ) * 360;
 	secHand.style.transform = `rotate(${secDegrees}deg)`;
+
+	const min = now.getMinutes();
+	const minDegrees = ( min / 60 ) * 360;
+	minHand.style.transform = `rotate(${minDegrees}deg)`;
+
+	const hour = now.getHours();
+	const hourDegrees = ( hour / 60 ) * 360;
+	hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
 
 setInterval(setDate, 1000);
