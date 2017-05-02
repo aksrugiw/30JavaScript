c4f1aa9a06ea0d370b610a25fb23ce53c35bad91
aksrugiw <aksrugiw@interia.pl>
2017-05-02 12:43:12 +0200

    02 - CSS Clock: Fix hour calculations, add 90 deg (start position):

--- CSS_Clock/script.js
+++ CSS_Clock/script.js
@@ -6,15 +6,15 @@ function setDate() {
 	const now = new Date();
 
 	const sec = now.getSeconds();
-	const secDegrees = ( sec / 60 ) * 360;
+	const secDegrees = (( sec / 60 ) * 360) + 90;
 	secHand.style.transform = `rotate(${secDegrees}deg)`;
 
 	const min = now.getMinutes();
-	const minDegrees = ( min / 60 ) * 360;
+	const minDegrees = (( min / 60 ) * 360) + 90;
 	minHand.style.transform = `rotate(${minDegrees}deg)`;
 
 	const hour = now.getHours();
-	const hourDegrees = ( hour / 60 ) * 360;
+	const hourDegrees = (( hour / 12 ) * 360) + 90;
 	hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
 
